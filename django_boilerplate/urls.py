61d79a17cf3de4efbe96c2c7df50251a46e34096
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-08-15 23:52:01 +0300

    urls.py: Add index url

--- django_boilerplate/urls.py
+++ django_boilerplate/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('rest_framework.urls'))
+    path('auth/', include('rest_framework.urls')),
+    path('', include(router.urls))
 ]
