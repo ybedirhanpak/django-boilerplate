ef1bef3c22d00b742473823c9615ceef090fb234
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-08-25 12:48:23 +0300

    app_user: Create user model

--- app_user/models.py
+++ app_user/models.py
@@ -1,1 +1,26 @@
-# Create your models here.
+from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from app_common.models import BaseModel
+from .managers import UserManager
+
+
+class User(PermissionsMixin, AbstractBaseUser, BaseModel):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=255, null=True, blank=True)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    weight = models.PositiveIntegerField(null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    is_visible = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+
+    objects = UserManager()
+
+    def __str__(self):
+        return '{}-{}'.format(self.username, self.email)
