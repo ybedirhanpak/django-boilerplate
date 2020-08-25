028841bc884fa6dffb02e0192d7f2b82bf32d352
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-08-25 12:23:44 +0300

    app_common/models: Create BaseModel

--- app_common/models.py
+++ app_common/models.py
@@ -1,1 +1,13 @@
-# Create your models here.
+from django.db import models
+
+
+class BaseModel(models.Model):
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{}-{}".format(type(self).__name__, self.id)
+
+    class Meta:
+        abstract = True
